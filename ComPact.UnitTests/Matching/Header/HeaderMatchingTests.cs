b02703509f2411f2d00f9f48c6eae58892a050a1
Bart Schotten <bschotten@gmail.com>
2019-08-13 07:00:41 +0200

    Adding the official testcases

---
+++ ComPact.UnitTests/Matching/Header/HeaderMatchingTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using ComPact.Models;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace ComPact.UnitTests.Matching.Header
+{
+    [TestClass]
+    public class HeaderMatchingTests
+    {
+        [TestMethod]
+        public void ShouldSuccessfullyExecuteAllTestcase()
+        {
+            var testcasesDir = Path.GetFullPath($"{AppContext.BaseDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}.." +
+                $"{Path.DirectorySeparatorChar}Matching{Path.DirectorySeparatorChar}Header{Path.DirectorySeparatorChar}Testcases{Path.DirectorySeparatorChar}");
+
+            var testcasesFile = Directory.GetFiles(testcasesDir);
+
+            var failedCases = new List<string>();
+
+            foreach(var file in testcasesFile)
+            {
+                var testcase = JsonConvert.DeserializeObject<TestCase>(File.ReadAllText(file));
+                var differences = testcase.Expected.Headers.Match(testcase.Actual.Headers, testcase.Expected.MatchingRules);
+                if (differences.Any() == testcase.Match)
+                {
+                    failedCases.Add(testcase.Comment);
+                }
+            }
+
+            Assert.AreEqual(0, failedCases.Count, "Failed cases: " + string.Join(Environment.NewLine, failedCases));
+        }
+
+        private class TestCase
+        {
+            public bool Match { get; set; }
+            public string Comment { get; set; }
+            public ExpectedHeaders Expected { get; set; }
+            public ActualHeaders Actual { get; set; }
+        }
+
+        private class ExpectedHeaders
+        {
+            public Headers Headers { get; set; }
+            public MatchingRuleCollection MatchingRules { get; set; }
+        }
+
+        private class ActualHeaders
+        {
+            public Headers Headers { get; set; }
+        }
+    }
+}
--- ComPact/Models/Headers.cs
+++ ComPact/Models/Headers.cs
@@ -47,5 +47,34 @@ namespace ComPact.Models
 
             return this.All(h => actualHeaders.Any(a => h.Key == a.Key && h.Value == a.Value));
         }
+
+        internal List<string> Match(Headers actualHeaders, MatchingRuleCollection matchingRules)
+        {
+            if (actualHeaders == null)
+            {
+                throw new System.ArgumentNullException(nameof(actualHeaders));
+            }
+
+            var differences = new List<string>();
+            foreach(var expectedHeader in this)
+            {
+                if (actualHeaders.TryGetValue(expectedHeader.Key, out var actualHeader))
+                {
+                    var expectedParts = expectedHeader.Value.ToLower().Split(";").Select(p => p.Trim()).ToList();
+                    var actualParts = actualHeader.ToLower().Split(";").Select(p => p.Trim()).ToList();
+                    if (!expectedParts.All(e => actualParts.Any(a => a.Equals(a == e))))
+                    {
+                        differences.Add($"Expected {expectedHeader.Value} for {expectedHeader.Key}, but was {actualHeader}");
+                    }
+
+                }
+                else
+                {
+                    differences.Add($"Expected a header named {expectedHeader.Key}, but was not found.");
+                }
+            }
+
+            return differences;
+        }
     }
 }
