5de020424db55554c980a65ee686080ba28c3941
Bart Schotten <bschotten@gmail.com>
2019-08-30 11:36:32 +0200

    Tests for pact broker integration

--- ComPact.ProviderTests/ProviderTests.cs
+++ ComPact.ProviderTests/ProviderTests.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 using ComPact.Mock.Consumer;
 using ComPact.ProviderTests.TestSupport;
+using ComPact.Tests.Shared;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace ComPact.ProviderTests
 {
@@ -13,7 +17,7 @@ namespace ComPact.ProviderTests
     public class ProviderTests
     {
         [TestMethod]
-        public void ShouldVerifyPact()
+        public async Task ShouldVerifyPact()
         {
             var baseUrl = "http://localhost:9494";
 
@@ -21,7 +25,7 @@ namespace ComPact.ProviderTests
 
             var cts = new CancellationTokenSource();
 
-            WebHost.CreateDefaultBuilder()
+            var hostTask = WebHost.CreateDefaultBuilder()
                 .UseKestrel()
                 .UseUrls(baseUrl)
                 .UseStartup<TestStartup>()
@@ -29,13 +33,14 @@ namespace ComPact.ProviderTests
 
             var buildDirectory = AppContext.BaseDirectory;
             var pactDir = Path.GetFullPath($"{buildDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}pacts{Path.DirectorySeparatorChar}");
-            mockConsumer.VerifyPact(pactDir + "recipe-consumer-recipe-service.json");
+            await mockConsumer.VerifyPact(pactDir + "recipe-consumer-recipe-service.json");
 
             cts.Cancel();
+            await hostTask;
         }
 
         [TestMethod]
-        public void ShouldVerifyMessagePact()
+        public async Task ShouldVerifyMessagePact()
         {
             FakeRecipeRepository recipeRepository = new FakeRecipeRepository();
 
@@ -51,7 +56,35 @@ namespace ComPact.ProviderTests
 
             var buildDirectory = AppContext.BaseDirectory;
             var pactDir = Path.GetFullPath($"{buildDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}pacts{Path.DirectorySeparatorChar}");
-            mockConsumer.VerifyPact(pactDir + "messageConsumer-messageProvider.json");
+            await mockConsumer.VerifyPact(pactDir + "messageConsumer-messageProvider.json");
+        }
+
+        [TestMethod]
+        public async Task ShouldPublishVerificationResults()
+        {
+            FakeRecipeRepository recipeRepository = new FakeRecipeRepository();
+
+            var recipeAddedProducer = new RecipeAddedProducer(recipeRepository);
+
+            var fakeHttpMessageHandler = new FakeHttpMessageHandler();
+
+            var config = new MockConsumerConfig
+            {
+                MessageProviderStateHandler = p => new MessageProviderStateHandler(recipeRepository).Handle(p),
+                MessageProducer = recipeAddedProducer.Send,
+                ProviderVersion = "1.0",
+                PublishVerificationResults = true,
+                PactBrokerClient = new HttpClient(fakeHttpMessageHandler) { BaseAddress = new Uri("http://local-pact-broker")}
+            };
+
+            var mockConsumer = new MockConsumer(config);
+
+            var buildDirectory = AppContext.BaseDirectory;
+            var pactDir = Path.GetFullPath($"{buildDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}pacts{Path.DirectorySeparatorChar}");
+            await mockConsumer.VerifyPact(pactDir + "messageConsumer-messageProvider.json");
+
+            var sentVerificationResults = JsonConvert.DeserializeObject<VerificationResults>(fakeHttpMessageHandler.SentRequestContent);
+            Assert.IsTrue(sentVerificationResults.Success);
         }
     }
 }
