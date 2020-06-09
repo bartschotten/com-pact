67051de9114a9958e275e8e719b24293d14134d0
slava_fiodorov <slava.fiodorov@tipalti.com>
2020-06-09 10:46:58 +0300

    Reduce duplicates on test code

--- ComPact.ConsumerTests/MessagePactTests.cs
+++ ComPact.ConsumerTests/MessagePactTests.cs
@@ -97,22 +97,6 @@ namespace ComPact.ConsumerTests
             // Check if handler has been called
             Assert.IsNotNull(handler.ReceivedRecipes.FirstOrDefault());
             Assert.AreEqual("A Recipe", handler.ReceivedRecipes.First().Name);
-
-            // Check if pact has been published and tagged
-            Assert.AreEqual("messageConsumer",
-                JsonConvert.DeserializeObject<Contract>(fakePactBrokerMessageHandler
-                        .GetStatus(HttpMethod.Put, "http://localhost:9292").SentRequestContents.First().Value).Consumer
-                    .Name);
-            Assert.IsTrue(fakePactBrokerMessageHandler.GetStatus(HttpMethod.Put, "http://localhost:9292").SentRequestContents.Last().Key.Contains("local"));
-
-            // Check if pact has been written to project directory.
-            var buildDirectory = AppContext.BaseDirectory;
-            var pactDir =
-                Path.GetFullPath(
-                    $"{buildDirectory}{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}pacts{Path.DirectorySeparatorChar}");
-            var pactFile = File.ReadAllText(pactDir + "messageConsumer-messageProvider.json");
-            Assert.AreEqual("messageConsumer", JsonConvert.DeserializeObject<Contract>(pactFile).Consumer.Name);
-            File.Delete(pactDir + "messageConsumer-messageProvider.json");
         }
 
         [TestMethod]
